11bdfea87bc360d27d2f0aa196ca78f91ebf438c
Bri Ward <me@briward.com>
2026-02-17 18:22:55 +0000

    Update framework version for compatibility and improve helper function.

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-// Copyright 2026, @raptor-http. All rights reserved. MIT license.
+// Copyright 2026, Raptor. All rights reserved. MIT license.
 
 import ErrorHandler from "./src/error-handler.ts";
 import helper from "./src/helper.ts";
--- src/helper.ts
+++ src/helper.ts
@@ -1,10 +1,31 @@
+// deno-lint-ignore-file no-explicit-any
+
 import type { ErrorHandler } from "@raptor/framework";
 
-import Handler from "./error-handler.ts";
+import Service from "./error-handler.ts";
 
-const handler = new Handler();
+const instance = new Service();
 
 /**
- * A convenient helper function for the error handler package.
+ * Prepare a new object which provides a great developer experience when
+ * registering the error handler middleware.
  */
-export default handler.handle as ErrorHandler;
+const errorHandler = new Proxy(instance.handle, {
+  get(target, prop, receiver) {
+    if (prop in instance) {
+      const value = (instance as any)[prop];
+
+      return typeof value === "function" ? value.bind(instance) : value;
+    }
+
+    return Reflect.get(target, prop, receiver);
+  },
+
+  set(_target, prop, value) {
+    (instance as any)[prop] = value;
+
+    return true;
+  },
+}) as ErrorHandler & Service;
+
+export default errorHandler;
